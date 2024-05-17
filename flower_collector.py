3730de301b1705cdea64435de1fd92533fb85cfd
Amarah <amarahahmed05@gmail.com>
2024-05-16 23:50:42 -0700

    partial solution to see actual flowers instead of circles

--- flower_collector.py
+++ flower_collector.py
@@ -19,10 +19,20 @@ character_color = BLUE
 character_pos = [WIDTH // 2, HEIGHT // 2]
 character_speed = 5
 
-# Flower settings
-flower_size = 20
-flower_color = PINK
-flower_pos = [random.randint(0, WIDTH - flower_size), random.randint(0, HEIGHT - flower_size)]
+
+# Load flower images
+flower_images = [
+    pygame.image.load('flower2.jpg'),
+    pygame.image.load('flower.jpg')
+]
+flower_rects = [img.get_rect() for img in flower_images]
+print(flower_rects)
+# Randomly position the first flower
+current_flower_index = 0
+flower_rects[current_flower_index].topleft = (
+    random.randint(0, WIDTH - flower_rects[current_flower_index].width),
+    random.randint(0, HEIGHT - flower_rects[current_flower_index].height)
+)
 
 running = True
 
@@ -43,14 +53,19 @@ while running:
 
 
     character_rect = pygame.Rect(character_pos[0], character_pos[1], character_size, character_size)
-    flower_rect = pygame.Rect(flower_pos[0], flower_pos[1], flower_size, flower_size)
-    if character_rect.colliderect(flower_rect):
-        flower_pos = [random.randint(0, WIDTH - flower_size), random.randint(0, HEIGHT - flower_size)]
+    # flower_rect = pygame.Rect(flower_pos[0], flower_pos[1], flower_size, flower_size)
+    if character_rect.colliderect(flower_rects[current_flower_index]):
+        current_flower_index = random.randint(0, len(flower_images) - 1)
+        flower_rects[current_flower_index].topleft = (
+            random.randint(0, WIDTH - flower_rects[current_flower_index].width),
+            random.randint(0, HEIGHT - flower_rects[current_flower_index].height)
+        )
 
     screen.fill(WHITE)
 
     pygame.draw.rect(screen, character_color, character_rect)
-    pygame.draw.ellipse(screen, flower_color, flower_rect)
+    screen.blit(flower_images[current_flower_index], flower_rects[current_flower_index])
+
 
     pygame.display.flip()
 
